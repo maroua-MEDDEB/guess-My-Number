44726ad8a4f2bf4fce99856b7cfdad4dccb60161
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-01-29 21:10:12 +0100

    refactoring code

--- script.js
+++ script.js
@@ -11,41 +11,35 @@ document.querySelector('.check').addEventListener('click',()=>{
     document.querySelector('.number').textContent = valueInputNumber; // afficher le nombre exact (le nombre récupéré de l'input)
 
     //message d'erreur
-    let messageError = document.querySelector('.message').textContent="Number is not betwwen 1 and 20!!!";
+    function msgErrorNotIncluNumber(){
+        document.querySelector('.message').textContent="Number is not betwwen 1 and 20!!!";
+         document.querySelector('.number').textContent = '?'; // afficher le nombre exact (le nombre récupéré de l'input)
+    } 
 
-    if(valueInputNumber <1){
-        messageError;
-        document.querySelector('.number').textContent = '?'; // afficher le nombre exact (le nombre récupéré de l'input)
-      
-    }else if(valueInputNumber>20){
-        messageError;
-    }else if(!valueInputNumber){
+    if(valueInputNumber < 1){
+        msgErrorNotIncluNumber();
+    }
+    else if(valueInputNumber > 20){
+        msgErrorNotIncluNumber();
+    }
+    else if(!valueInputNumber){
         document.querySelector('.message').textContent="the value is not a Number!!!";
         document.querySelector('.number').textContent = '?'; // afficher le nombre exact (le nombre récupéré de l'input)
-
-
-    }else{
+    }
+    else{
         document.querySelector('.message').textContent ="start guessing...";
         
-        if(valueInputNumber > guessNumber){
-            document.querySelector('.message').textContent ="Your Number is Low";
-            score = score - 1;
-            document.querySelector('.score').textContent= score;
-            if(score<0){
-                document.querySelector('.score').textContent= "0";
-                document.querySelector('.message').textContent= "Sorry, you Lost!";
-            }
-             }else if(valueInputNumber < guessNumber){
-            document.querySelector('.message').textContent ="Your Number is Less";
+        if(valueInputNumber !== guessNumber){
+            document.querySelector('.message').textContent = valueInputNumber > guessNumber ? "Your Number is Low" : "Your Number is Less";
             score = score - 1;
             document.querySelector('.score').textContent= score;
             if(score<0){
                 document.querySelector('.score').textContent= "0";
                 document.querySelector('.message').textContent= "Sorry, you Lost!";
                 document.querySelector('body').style.backgroundColor='red'; // modifier la couleur du body
-
             }
-        }else{
+        }
+        else{
             document.querySelector('.message').textContent ="greate, Wright Number!"; //message de comfirmation
             document.querySelector('.number').textContent = valueInputNumber; // afficher le nombre exact (le nombre récupéré de l'input)
             document.querySelector('.number').style.width = '30rem';
